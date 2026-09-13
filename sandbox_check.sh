b05d9f4e5683a727b1b822a951c4227dfa13eb46
hppetersteve <hppetersteve@users.noreply.github.com>
2026-09-13 18:41:39 -0300

    chore: remove test scripts, surplus tools and unused runtime assets
    Deleted: sandbox_check.sh, sandbox_mass_test.sh, test_path.c,
    tools/diagnose_tmp.{c,sh}, tools/safe_confinement_probe.{c,sh},
    afl++_4.09c-1ubuntu3_amd64.deb, assets/logo.{png,svg},
    runtime/ubuntu-24.04-amd64.tar.gz (replaced by ubuntu24_04.squashfs),
    SANDBOX_GUIDE.md. Moved to Nuk4sd_scripts_e_extras outside the repo.

--- sandbox_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "--- ROOT DIRECTORY ---"
-ls -la /
-echo -e "\n--- HOME DIRECTORY ---"
-ls -la /home/pedro
-echo -e "\n--- PROCESSES ---"
-ps -ef
-echo -e "\n--- MOUNTS ---"
-mount | grep -E "/home|/proc|/sys"
